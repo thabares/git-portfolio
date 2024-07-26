7ee30deee9878b5e2742b151b560f2c5a09c5241
Thabares Nazeer Basha <thabaresnazeer.1995@gmail.com>
2024-07-26 21:37:07 +0530

    navbar changes updated.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
 import Home from './pages/Home';
+import { Route, Routes } from 'react-router-dom';
+import Overview from './pages/Overview';
+import Contact from './pages/Contact';
+import Projects from './pages/Projects';
 
 const App = () => {
-  return <Home />;
+  return (
+    <div>
+      <Home />
+      <Routes>
+        <Route path='/' element={<Overview />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+    </div>
+  );
 };
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,81 @@
-import AppHeader from '../components/AppHeader';
+import './appHeader.css';
+import headerUserIcon from '../avatar.png';
+import { IoBookOutline } from 'react-icons/io5';
+import { MdOutlineContactPhone } from 'react-icons/md';
+import { GoProjectSymlink } from 'react-icons/go';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 const Home = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  console.log(location.pathname);
+  const handleRedirect = (pageUrl) => {
+    return navigate(pageUrl);
+  };
   return (
-    <div>
-      <AppHeader />
-    </div>
+    <header className='appHeaderContainer'>
+      <div className='headerWrapper'>
+        <div className='headerContainer'>
+          <a className='userNameContainer'>
+            <h5 className='userName'>thabares</h5>
+          </a>
+        </div>
+        <div className='appHeaderUser'>
+          <button className='appHeaderUserButton'>
+            <span className='button-content'>
+              <span className='button-label'>
+                <img
+                  src={headerUserIcon}
+                  alt='headerUserIcon'
+                  className='avatar'
+                  size={30}
+                  height={30}
+                  width={30}
+                />
+              </span>
+            </span>
+          </button>
+        </div>
+      </div>
+      <div className='appHeaderLocal'>
+        <nav className='overflow-hidden underlineNav'>
+          <ul className='underLineNavBody list-style-none'>
+            <li className='inlineFlex'>
+              <a
+                href='/'
+                className={`underlineNavItem no-wrap 
+                  ${location.pathname === '/' && `selected`}`}
+              >
+                <IoBookOutline className='overviewIcon' />
+                <span>Overview</span>
+              </a>
+            </li>
+
+            <li className='inlineFlex'>
+              <a
+                href='/projects'
+                className={`underlineNavItem no-wrap 
+                  ${location.pathname === '/projects' && `selected`}`}
+              >
+                <GoProjectSymlink className='overviewIcon' />
+                <span>Projects</span>
+              </a>
+            </li>
+
+            <li className='inlineFlex'>
+              <a
+                href='/contact'
+                className={`underlineNavItem no-wrap 
+                  ${location.pathname === '/contact' && `selected`}`}
+              >
+                <MdOutlineContactPhone className='overviewIcon' />
+                <span>Contacts</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
   );
 };
 export default Home;
