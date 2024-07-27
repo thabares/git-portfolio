e1cbdea4bee15d73fe500a7ca8f5b559c14b37f2
Thabares Nazeer Basha <thabaresnazeer.1995@gmail.com>
2024-07-27 09:20:17 +0530

    Adding a template to use the ages as reusable components.

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import { Route, Routes } from 'react-router-dom';
 import Overview from './pages/Overview';
 import Contact from './pages/Contact';
 import Projects from './pages/Projects';
+import Template from './template/Template';
 
 const App = () => {
   return (
     <div>
-      <Home />
       <Routes>
-        <Route path='/' element={<Overview />} />
-        <Route path='/projects' element={<Projects />} />
-        <Route path='/contact' element={<Contact />} />
+        <Route element={<Template />}>
+          <Route path='/' element={<Overview />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/contact' element={<Contact />} />
+        </Route>
       </Routes>
     </div>
   );
---
+++ src/template/Template.jsx
@@ -0,0 +1,10 @@
+import Home from '../pages/Home';
+
+const Template = () => {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+};
+export default Template;
